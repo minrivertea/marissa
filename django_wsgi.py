9d8e18b13d1ebe4b7328e2d8855fd18d3f6c7db9
Chris West <chris@minrivertea.com>
2011-07-24 09:44:29 +0800

    changes

--- django_wsgi.py
+++ django_wsgi.py
@@ -2,6 +2,6 @@ import sys
 import os
 import django.core.handlers.wsgi
 
-sys.path.append('/var/lib/django/')
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'marissa.settings'
 application = django.core.handlers.wsgi.WSGIHandler()
