c9fbf24c785f7b0638ced6d1024216945b38d423
Chris <chris@fry-it.com>
2011-11-22 09:15:32 +0800

    changes

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,6 @@ class BlogEntry(models.Model):
     slug = models.SlugField(max_length=80)
     promo_image = models.ImageField(upload_to='images/blog-photos', blank=True, null=True)
     date_added = models.DateField()
-    is_gallery = models.BooleanField(default=False)
     is_draft = models.BooleanField(default=True)
     title = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
